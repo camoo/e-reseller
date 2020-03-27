ef82e593d576ef1d9f8f862f7e70763f927a7c4d
Camoo Dev <etn@camoo.sarl>
2020-03-27 19:03:25 +0100

    WIP

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,3 +2,6 @@
 require_once dirname(__DIR__). '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/config/paths.php';
 require_once CORE_PATH . 'config' . DS . 'bootstrap.php';
+if (file_exists(CONFIG. '.env') && is_readable(CONFIG.'.env')) {
+    (new \josegonzalez\Dotenv\Loader(CONFIG.'.env'))->parse()->define();
+}
