f9d78860d8cac20fc5c8126ef651390cca9f696b
Camoo Dev <etn@camoo.sarl>
2020-10-27 08:58:09 +0100

    Contact us improved

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use CAMOO\Mailer\Mailer;
+use CAMOO\Utils\Configure;
 
 /**
  * Class ContactController
@@ -14,19 +15,44 @@ class ContactController extends AppController
     public function overview()
     {
         if ($this->request->is('post')) {
-            $message = $this->request->getData('message');
-            $subject = $this->request->getData('subject');
-            $name = $this->request->getData('name');
-            $replyTo = $this->request->getData('email');
-            $oEmail = new Mailer();
-            $oEmail->addTo($adminEmail)
-                    ->setSubject($subject)
-                    ->addReplyTo($replyTo)
-                    ->setBody($message);
-            $oEmail->send();
-            $this->request->Flash->success('Merci! Nous vous contacterons dès que possible');
+            $email = $this->request->getData('email');
+            if ($this->isValidEmail($email)) {
+                $this->sendEmail();
+                $this->request->Flash->success('Merci! Nous vous contacterons dès que possible');
+            }
+            $this->request->Flash->error('Votre demande ne peut pas être traitée pour le moment');
         }
 
         $this->render();
+    }
+
+    /**
+     * @param string $email
+     *
+     * @return bool
+     */
+    private function isValidEmail(string $email) : bool
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+    }
+
+    protected function sendEmail() : void
+    {
+        $message = $this->request->getData('message');
+        $subject = $this->request->getData('subject');
+        $name = $this->request->getData('name');
+        $replyTo = $this->request->getData('email');
+        $oEmail = new Mailer();
+        $baseUrl = $this->request->getEnv('HTTP_HOST');
+
+        $message .= "\n\n\n*************************************************\n\n" . Configure::read('RESELLER_SITE.title_for_layout') ."\n" .
+            $baseUrl. "\n\n Contact us \n\n*************************************************";
+
+		$adminEmail = Configure::read('RESELLER_SITE.contact_email');
+        $oEmail->addTo($adminEmail)
+                    ->setSubject($subject)
+                    ->addReplyTo($replyTo)
+                    ->setBody($message);
+        $oEmail->send();
     }
 }
